80a3de41f2596976753114cd198da7574e82f193
Greg Reichow <gdr3941@me.com>
2022-03-06 09:23:54 -0800

    working create rocks

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,74 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/System.hpp>
 #include <iostream>
+#include <vector>
+#include "SFML/System/Vector2.hpp"
+#include "SFML/Window/Window.hpp"
+#include "range/v3/algorithm/for_each.hpp"
+#include <range/v3/algorithm.hpp>
+#include <functional>
+#include "util.h"
+
+using namespace std::placeholders;
+namespace r = ranges;
+
+// Initial Extents for new Rocks
+constexpr float kInitialPosExtent = 10.0f;
+constexpr float kInitialVelExtent = 2.0f;
+constexpr float kInitialRadiusMix = 0.01f;
+constexpr float kInitialRadiusMax = 2.0f; 
+
+struct Rock {
+    sf::Vector2f pos {};
+    sf::Vector2f vel {};
+    float radius {0.0f};
+
+};
+
+void print(const Rock& rock) {
+    fmt::print("Rock: pos: {},{} vel: {},{} radius: {}\n",
+               rock.pos.x, rock.pos.y,
+               rock.vel.x, rock.vel.y,
+               rock.radius);
+}
+
+using Rocks = std::vector<Rock>;
+
+// struct World {
+//     Rocks rocks;
+// };
+
+Rocks createNewRocks(size_t num)
+{
+    Rocks rocks(num);
+    for (auto& rock : rocks) {
+        rock.pos.x = util::f_rand(-kInitialPosExtent, kInitialPosExtent);
+        rock.pos.y = util::f_rand(-kInitialPosExtent, kInitialPosExtent);
+        rock.vel.x = util::f_rand(-kInitialVelExtent, kInitialVelExtent);
+        rock.vel.y = util::f_rand(-kInitialVelExtent, kInitialVelExtent);
+        rock.radius = util::f_rand(kInitialRadiusMix, kInitialRadiusMax);
+    }
+    return rocks;
+}
+
+// Trying Alternate Functional vs Imperative approaches for position updates
+
+void updatePosition(Rock& rock, float timeStep)
+{
+    rock.pos += rock.vel * timeStep;
+}
+
+auto updatePositionFor(float timeStep)
+{
+    return std::bind(updatePosition, _1, timeStep);
+
+}
+void updatePositions(Rocks& rocks, float timeStep)
+{
+    for (auto& rock : rocks) {
+        rock.pos += rock.vel * timeStep;
+    }
+}
 
 void run()
 {
@@ -12,6 +80,14 @@ void run()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Gravity");
     window.setFramerateLimit(60);
 
+    Rocks rocks {createNewRocks(10)};
+    r::for_each(rocks, print);
+    // updatePositions(rocks, 1.0f);
+    r::for_each(rocks, updatePositionFor(1.0f));
+    fmt::print("---\n");
+    r::for_each(rocks, print);
+    
+    
     sf::Clock clock;
     while (window.isOpen()) {
         sf::Event event;
